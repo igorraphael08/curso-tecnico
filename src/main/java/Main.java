81c882a3ce1ac78915203c9ed7ad78307e90dedd
igor raphael <169068315+igorraphael08@users.noreply.github.com>
2024-05-13 19:02:18 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("CURSO TÉCNICO");
+    System.out.println("----------------------------------");
+    System.out.println("desde que começei o curso técnico não to tendo nenhuma dificuldade , as aulas são boas e não tão dificil, sou muito grato por poder estar no curso técnico, pois sei que vou ter vantagens no mercado de trabalho, meus familiares torçeram muito pra que eu entrasse no curso técnico");
+    System.out.println("ainda estamos no primeiro trimeste, mas ja percebi a diferença do ensino médio para o fundamental, e tamém estou me saindo muito bem em certas matérias, escolhi desenvolvimento de sistemas pois queria aprender a mexer com programação, minha área sempre foi pro lado da computação e etc");
+    System.out.println("enfim, eu to se sentindo confortável com o aprendizado que estou tento no curso técnico no colégio lindaura.");
+  }
+
+  
+}
